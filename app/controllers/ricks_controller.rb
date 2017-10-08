ee1740d74f94707dee4e268c515c5ceaa70a52ba
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-08 16:48:27 +0200

    create new rick page

--- app/controllers/ricks_controller.rb
+++ app/controllers/ricks_controller.rb
@@ -1,8 +1,27 @@
 class RicksController < ApplicationController
 
   def index
-    @ricks = Rick.all
+    @rick = Rick.all
   end
 
+  def show
+    @rick = Rick.find(params[:id])
+  end
+
+  def new
+    @rick = Rick.new
+  end
+
+  def create
+    rick_params = params.require(:rick).permit(:name, :age, :dimension, :drunk)
+
+    @rick = Rick.new(rick_params)
+
+    if @rick.save
+       redirect_to @rick
+    else
+       render 'new'
+    end
+  end
 
 end
